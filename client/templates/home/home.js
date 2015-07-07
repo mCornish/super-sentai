6db7e9fcc68ba572c49668afc6deb3f90b31089b
Mike Cornish <cornishmw@gmail.com>
2015-07-07 18:46:44 -0400

    First commit

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -1,28 +1,14 @@
 Template.home.helpers({
     name: function() {
         return Meteor.user().username || Meteor.user().profile.name;
-    },
-    avatar: function() {
-        return Meteor.user().profile.image || 'http://thesocietypages.org/socimages/files/2009/05/vimeo.jpg';
-    },
-    haveRecommendations: function() {
-        var gifts = Router.current().data().gifts;
-        return gifts.count() > 0;
-    },
-    haveWants: function() {
-        var gifts = Router.current().data().wants;
-        return gifts.count() > 0;
     }
 });
 
 Template.home.events({
-    'click [data-hook="log-out"]': function(e) {
+    'click [data-hook="start"]': function(e) {
         e.preventDefault();
-
-        Meteor.logout(function(error) {
-            if (error) {
-                alert(error.reason);
-            }
-        });
+    },
+    'click [data-hook="options"]': function(e) {
+        e.preventDefault();
     }
 });
