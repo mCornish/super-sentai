00f848db2e965746894b956efda3c417704260b9
Mike Cornish <cornishmw@gmail.com>
2015-07-14 09:53:22 -0400

    Hooked up name input

--- client/templates/characters/characters.js
+++ client/templates/characters/characters.js
@@ -1,3 +1,11 @@
-Template.characters.events({
+Template.characters.onRendered( function() {
+    Session.set('playerName', null);
+});
 
+Template.characters.events({
+    'click [data-hook=name-input]': function(e) {
+        var name = $('[data-hook=name]').val();
+
+        Session.set('playerName', name);
+    }
 });
